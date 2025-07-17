f937344218dd9254d4a98501cf7c03611f988576
liyuxuan <liyuxuan3003@outlook.com>
2025-07-17 22:49:52 +0800

    配合code测试

---
+++ Notebook/code/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    // hello world
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
---
+++ Notebook/code/test.py
@@ -0,0 +1,4 @@
+import os
+
+# hello world
+print("hello world")
